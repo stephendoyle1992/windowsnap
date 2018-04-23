9324db08e15eb0e0f45bd64660226f007a660478
stephendoyle1992 <doyles8@mymacewan.ca>
2018-04-23 14:38:06 -0600

    init xcode project

---
+++ windowsnap/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  windowsnap
+//
+//  Created by Stephen Doyle on 2018-04-23.
+//  Copyright © 2018 Stephen Doyle. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
